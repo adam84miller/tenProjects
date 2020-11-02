ba4694e0d299bcc71ad6ed756fa5275695d199d1
adam84miller <adam.84.miller@gmail.com>
2020-11-02 10:02:17 -0700

    added new files

---
+++ script.js
@@ -0,0 +1,88 @@
+let secondsRemaining;
+let intervalHandle;
+
+function resetPage(){
+
+	document.getElementById("inputArea").style.display = "block";
+
+}
+
+function tick(){
+	// grab the h1
+	let timeDisplay = document.getElementById("time");
+
+	// turn the seconds into mm:ss
+	let min = Math.floor(secondsRemaining / 60);
+	let sec = secondsRemaining - (min * 60);
+
+	//add a leading zero (as a string value) if seconds less than 10
+	if (sec < 10) {
+		sec = "0" + sec;
+	}
+
+	// concatenate with colon
+	let message = min.toString() + ":" + sec;
+
+	// now change the display
+	timeDisplay.innerHTML = message;
+
+	// stop is down to zero
+	if (secondsRemaining === 0){
+		alert("Done!");
+		clearInterval(intervalHandle);
+		resetPage();
+	}
+
+	//subtract from seconds remaining
+	secondsRemaining--;
+
+}
+
+function startCountdown(){
+
+	function resetPage(){
+		document.getElementById("inputArea").style.display = "block";
+	}
+
+	// get countents of the "minutes" text box
+	let minutes = document.getElementById("minutes").value;
+	
+	// check if not a number
+	if (isNaN(minutes)){
+		alert("Please enter a number");
+		return; // stops function if true
+	}
+
+	// how many seconds
+	secondsRemaining = minutes * 60;
+	
+	//every second, call the "tick" function
+	// have to make it into a variable so that you can stop the interval later!!!
+	intervalHandle = setInterval(tick, 1000);
+
+	// hide the form
+	document.getElementById("inputArea").style.display = "none";
+
+
+}
+
+window.onload = function(){
+
+	// create input text box and give it an id of "min"
+	let inputMinutes = document.createElement("input");
+	inputMinutes.setAttribute("id", "minutes");
+	inputMinutes.setAttribute("type", "text");
+	
+	//create a button
+	var startButton = document.createElement("input");
+	startButton.setAttribute("type","button");
+	startButton.setAttribute("value","Start Countdown");
+	startButton.onclick = function(){
+		startCountdown();
+	};
+
+	//add to the DOM, to the div called "inputArea"
+	document.getElementById("inputArea").appendChild(inputMinutes);
+	document.getElementById("inputArea").appendChild(startButton)		
+
+}
